979170de0bf47af75653fddb0dcaea0fa72dd1c6
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-04-07 19:10:42 +0200

    refactor

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -5,10 +5,12 @@
             var result = 0;
             if (string.IsNullOrEmpty(input)) return result;
 
+            var separators = new char[] { ',', '\n' };
             if (input.Contains("//")) {
+                separators.ToList().Add(';');
                 input = input.Substring(input.IndexOf("//") + 3);
             }
-            var numbers = input.Split(',', '\n',';');
+            var numbers = input.Split(separators);
             foreach (var number in numbers) {
                 if (number.Length > 0) {
                     result += int.Parse(number);
