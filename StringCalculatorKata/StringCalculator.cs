8263ae919a0b41d08d90670b4e80073e4a25b682
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-04-07 18:34:18 +0200

    green:string_one_comma_two_returns_number_three

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,11 +1,14 @@
 ﻿namespace StringCalculatorKata {
     public class StringCalculator {        
 
-        public int Add(string number) {
+        public int Add(string input) {
             var result = 0;
-            if (string.IsNullOrEmpty(number))
+            if (string.IsNullOrEmpty(input))
                 result = 0;
-            result += int.Parse(number);
+            var numbers = input.Split(',');
+            foreach (var number in numbers) {
+                result += int.Parse(number);
+            }
             return result;
         }
     }
